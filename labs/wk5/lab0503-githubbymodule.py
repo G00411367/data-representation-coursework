21ceba6714f5dbff7627d4ea83d21cabe8fcc9b5
IoDom <G00411367@gmit.ie>
2023-10-30 22:39:28 +0000

    lab0503

---
+++ labs/wk5/lab0503-githubbymodule.py
@@ -0,0 +1,30 @@
+import requests
+from github import Github
+from config import config as cfg
+
+apikey = cfg["githubkeyc"]
+#apikey ="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"
+
+g = Github(apikey)
+
+#for repo in g.get_user().get_repos():
+   #print(repo.name)
+
+repo = g.get_repo("G00411367/testpygithub") 
+#print(repo.clone_url) 
+
+fileInfo = repo.get_contents("test.txt")
+urlOfFile = fileInfo.download_url
+#print(urlOfFile)
+
+response = requests.get(urlOfFile)
+contentOfFile = response.text
+#print(contentOfFile)
+
+newContents = contentOfFile + "more stuff \n"
+print(newContents)
+
+gitHubResponse = repo.update_file("test.txt", "updated by prog", newContents, fileInfo.sha)
+print(gitHubResponse)
+
+#https://github.com/G00411367/testpygithub/blob/main/test.txt
