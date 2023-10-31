31d8075f5a34e9035dbf78abb69b80df34a1aeaa
IoDom <G00411367@gmit.ie>
2023-10-31 01:03:37 +0000

    assignment4

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -2,7 +2,7 @@ import requests
 from github import Github
 from config import config as cfg
 
-apikey = cfg["githubkeyc"]
+apikey = cfg["githubkey"]
 
 #apikey = ""
 
@@ -11,10 +11,16 @@ g = Github(apikey)
 repo = g.get_repo("G00411367/data-representation-coursework") 
 #print(repo.clone_url) 
 
-fileInfo = repo.get_contents("test.txt")
+fileInfo = repo.get_contents("assignments/andrewFile.txt")
 urlOfFile = fileInfo.download_url
 #print(urlOfFile)
 
 response = requests.get(urlOfFile)
 contentOfFile = response.text
 #print(contentOfFile)
+
+newContents = contentOfFile.replace("Andrew", "Ioan")
+print(newContents)
+
+gitHubResponse = repo.update_file("assignments/andrewFile.txt", "replace Andrew", newContents, fileInfo.sha)
+print(gitHubResponse)
